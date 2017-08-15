8ff9e76765b625cb5f4740214c458417404998a1
goldboat90 <tyler.rymer@gmail.com>
2017-08-14 22:50:42 -0600

    added sidebar menu. Still in progress

--- public/js/controllers/mainCtrl.js
+++ public/js/controllers/mainCtrl.js
@@ -1,6 +1,23 @@
 angular.module('timbercrate')
   .controller('mainCtrl', function($scope, mainService) {
 
+    $scope.openSidebar = function() {
+      console.log("clicked");
+      document.body.classList.add("no-scroll");
+      var d = document.getElementById("nav-sidebar");
+      var e = document.getElementById("sidebar-overlay");
+      d.className += " sidebar-active";      
+      e.className += " overlay-active";
+    }
+
+    $scope.closeSidebar = function() {
+      document.body.classList.remove("no-scroll");      
+      var d = document.getElementById("nav-sidebar");
+      var e = document.getElementById("sidebar-overlay");
+      d.classList.remove("sidebar-active");
+      e.classList.remove("overlay-active");
+    }
+
     mainService.getFeatured()
       .then(response => {
         $scope.featured = response;
