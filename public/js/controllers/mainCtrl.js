a395b69140200bba3e81cd176ff0cd34a9f4cd48
goldboat90 <tyler.rymer@gmail.com>
2017-08-26 15:47:08 -0600

    added anchor scrolling to contact form for nav and carousel button

--- public/js/controllers/mainCtrl.js
+++ public/js/controllers/mainCtrl.js
@@ -1,10 +1,9 @@
 angular.module('timbercrate')
-  .controller('mainCtrl', function($scope, mainService) {
+  .controller('mainCtrl', function($scope, mainService, $state) {
     
     $scope.userClicked = false;
 
     $scope.openSidebar = function() {
-      console.log("clicked");
       document.body.classList.add("no-scroll");
       var d = document.getElementById("nav-sidebar");
       var e = document.getElementById("sidebar-overlay");
@@ -23,23 +22,50 @@ angular.module('timbercrate')
     mainService.getFeatured()
       .then(response => {
         $scope.featured = response;
-        console.log($scope.featured);
       })
 
     $scope.enlargeImg = function (fileName) {
       $scope.userClicked = false;
       $scope.imgName = fileName;
-      console.log($scope.userClicked);
       $("#fw-container").css("top", window.scrollY);
       $("body").addClass("no-scroll");
-      console.log(window.scrollY);
       $("#fw-container").addClass("full-width-img");
     }
+
     $scope.closeImg = function () {
       $("body").removeClass("no-scroll");    
       $("#fw-container").removeClass("full-width-img");
       $scope.userClicked = true;
-      console.log($scope.userClicked);      
     }
+
+    $scope.anchorClose = function() {
+      document.body.classList.remove("no-scroll");      
+      var d = document.getElementById("nav-sidebar");
+      var e = document.getElementById("sidebar-overlay");
+      d.classList.remove("sidebar-active");
+      e.classList.remove("overlay-active");           
+      if ($state.current.name === 'products') {
+        $state.go('home', {whenever: 'whatever'}).then(function() {
+          $('html, body').animate({
+            scrollTop: $("#doContact").offset().top + 50
+        }, 500);
+        });
+        
+      }
+      else {
+        $('html, body').animate({
+          scrollTop: $("#doContact").offset().top + 50
+      }, 500);
+      }
+    }
+
+    $scope.toContact = function() {
+      $('html, body').animate({
+        scrollTop: $("#doContact").offset().top + 50
+    }, 500);
+    }
+    
+   
+
 
   });
